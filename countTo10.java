e8cd0fa9dc1ec72f57c8b2b4b71625da23cce7f4
Minh Cuong Do <mcslender97@gmail.com>
2021-10-19 12:42:27 -0700

    Remove thread.sleep for 30 seconds on the src code

--- countTo10.java
+++ countTo10.java
@@ -1,15 +1,13 @@
 public class countTo10 {
     public static void main(String args[]) {
-        try{
-            Thread.sleep(30000);
+      
+       
             for (int i = 1; i <= 10; i++) {
                 System.out.println(
                     i
                 );
-            }
-        }catch(InterruptedException ex){
-            ex.printStackTrace();
-        }
+            
+
 
     }
 }
